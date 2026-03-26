f04ba19014926ab0c1a59d0fdbdb739fddef9aed
Titiwat <titiwat.te06@gmail.com>
2026-03-26 17:18:35 +0700

    26

---
+++ arrow-function.js
@@ -0,0 +1,2 @@
+const greet = (name,role) => `Hello, my name is ${name} I work as a ${role}`;
+console.log(greet("Tay","Coder")); // Hello, my name Tay
---
+++ declarative-functions.js
@@ -0,0 +1,17 @@
+// function greet() {
+//     const james = "Hello World"
+//     return james;
+// }
+
+// greet();
+
+// console.log(greet());
+
+// function greet(name) {
+
+//     const role = "Coder";
+
+//     return `Hello, my name ${name}. I work as ${role}`;
+// }
+
+// console.log(greet("Tay"))
